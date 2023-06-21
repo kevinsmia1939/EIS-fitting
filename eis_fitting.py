dacf274328cfee31f3b2a8f585fd5466081ec9fd
kevin <kevin_tee@pm.me>
2023-06-21 12:05:44 +0200

    update with fitted param array

--- eis_fitting.py
+++ eis_fitting.py
@@ -1,19 +1,17 @@
 from impedance import preprocessing
 from impedance.models.circuits import CustomCircuit
 import matplotlib.pyplot as plt
-from impedance.visualization import plot_nyquist
 import pandas as pd
 from matplotlib.pyplot import figure
 import numpy as np
+
 figure(figsize=(7, 7), dpi=100)
 df = pd.read_csv('eis.txt', delimiter='\t')
 frequencies = df['Frequency']
 Zre = df['Zre']
 Zim = -df['Zim']
 Z = np.vectorize(complex)(Zre, Zim)
-plt.plot(np.real(Z), -np.imag(Z),':')
-
-
+plt.plot(np.real(Z), -np.imag(Z),'o')
 
 circuit = 'L0-R1-p(R2,CPE0)-p(R3,CPE1)-W0'
 initial_guess = [1.3e-8, 3.8e-4,5.9e-5, 7.7e1,8e-1,2.9e-4,2.4e5,1,2.8e-5]
@@ -21,8 +19,10 @@ circuit = CustomCircuit(circuit, initial_guess=initial_guess)
 circuit.fit(frequencies, Z)
 Z_fit = circuit.predict(frequencies)
 
-plt.plot(np.real(Z_fit), -np.imag(Z_fit),':')
-print(circuit)
-
+plt.plot(np.real(Z_fit), -np.imag(Z_fit),'-')
 plt.legend(['Data', 'Fit'])
 plt.show()
+
+print(circuit) # Fitted parameters
+print(circuit.parameters_) # Fitted parameters as array
+
